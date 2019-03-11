846b7eeafe51fd94645a7d7368c327967a5af375
Farhad Bharucha <36323915+Farhad1234@users.noreply.github.com>
2019-03-11 18:15:17 +0530

    updated index.js

--- server/index.js
+++ server/index.js
@@ -3,15 +3,15 @@ var s = new server({port: 5001});
 
 
 s.on('connection',function(ws){
-	 ws.on('message',function(message){
-	 	console.log("Received: "+ message);
+	 ws.on('message',function(coods){
+	 	console.log("Received: Accident At"+ coods);
 	// var obj = {lat:25.5941, lng:85.1376};
 		s.clients.forEach(function e(client){
 			if(client==ws)
-				client.send(message);
+				client.send(coods);
 		});
 		ws.on('close',function(){
-			console.log("I lost a client");
+			console.log("client disconnected");
 		});
 		//ws.send(JSON.stringify(obj));
 	 });
