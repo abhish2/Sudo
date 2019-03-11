157854f4817bc54fa1301ea220375f86e72e9b21
Farhad Bharucha <36323915+Farhad1234@users.noreply.github.com>
2019-03-11 21:46:53 +0530

    Create accelarate.ino

---
+++ Beta/accelarate.ino
@@ -0,0 +1,68 @@
+#include <Wire.h>
+#include <MPU6050.h>
+
+MPU6050 mpu;
+float acc=0;
+void setup() 
+{
+  Serial.begin(115200);
+  pinMode(13,OUTPUT);
+  digitalWrite(13,0);
+  Serial.println("Initialize MPU6050");
+
+  while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
+  {
+    Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
+    delay(500);
+  }
+
+  checkSettings();
+}
+
+void checkSettings()
+{
+  Serial.println();
+  
+  Serial.print(" * Sleep Mode:            ");
+  Serial.println(mpu.getSleepEnabled() ? "Enabled" : "Disabled");
+  
+  Serial.print(" * Clock Source:          ");
+  switch(mpu.getClockSource())
+  {
+    case MPU6050_CLOCK_KEEP_RESET:     Serial.println("Stops the clock and keeps the timing generator in reset"); break;
+    case MPU6050_CLOCK_EXTERNAL_19MHZ: Serial.println("PLL with external 19.2MHz reference"); break;
+    case MPU6050_CLOCK_EXTERNAL_32KHZ: Serial.println("PLL with external 32.768kHz reference"); break;
+    case MPU6050_CLOCK_PLL_ZGYRO:      Serial.println("PLL with Z axis gyroscope reference"); break;
+    case MPU6050_CLOCK_PLL_YGYRO:      Serial.println("PLL with Y axis gyroscope reference"); break;
+    case MPU6050_CLOCK_PLL_XGYRO:      Serial.println("PLL with X axis gyroscope reference"); break;
+    case MPU6050_CLOCK_INTERNAL_8MHZ:  Serial.println("Internal 8MHz oscillator"); break;
+  }
+  
+  Serial.print(" * Accelerometer:         ");
+  switch(mpu.getRange())
+  {
+    case MPU6050_RANGE_16G:            Serial.println("+/- 16 g"); break;
+    case MPU6050_RANGE_8G:             Serial.println("+/- 8 g"); break;
+    case MPU6050_RANGE_4G:             Serial.println("+/- 4 g"); break;
+    case MPU6050_RANGE_2G:             Serial.println("+/- 2 g"); break;
+  }  
+
+  Serial.print(" * Accelerometer offsets: ");
+  mpu.getAccelOffsetX();
+  mpu.getAccelOffsetY();
+  mpu.getAccelOffsetZ();
+  
+  Serial.println();
+}
+
+void loop()
+{
+  Vector rawAccel = mpu.readRawAccel();
+  Vector normAccel = mpu.readNormalizeAccel();
+
+  acc=pow(((normAccel.XAxis*normAccel.XAxis)+(normAccel.YAxis*normAccel.YAxis)+(normAccel.ZAxis*normAccel.ZAxis)),0.5);
+  Serial.println(acc);
+  if(acc>24)
+  digitalWrite(13,1);
+  delay(100);
+}
